a90ff2d0dade2710f0aa7d7dc0479fc295702f5b
michal <michal@michal-laptop>
2009-01-23 03:48:20 +0100

    comment to main added

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv); 
+    QApplication app(argc, argv);
 
     GeoFracGUI w;
     w.show();
     app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
     return app.exec();
 }
+
+// new comment
